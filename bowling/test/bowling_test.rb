99c9bbb631eee247dea842f80fecfb8bb8ed592d
Sandi Metz <sandi@sandimetz.com>
2018-08-25 07:42:29 -0400

    Add test for initial spare in complete game.

--- bowling/test/bowling_test.rb
+++ bowling/test/bowling_test.rb
@@ -14,4 +14,9 @@ class BowlingTest < Minitest::Test
     rolls = [1] * 20
     assert_equal 20, Bowling.new(rolls).score
   end
+
+  def test_one_spare
+    rolls = [5, 5, 3] + [0] * 17
+    assert_equal 16, Bowling.new(rolls).score
+  end
 end
