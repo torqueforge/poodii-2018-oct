fe60f89daac506ec6f66cd55feee055a801684fa
Sandi Metz <sandi@sandimetz.com>
2018-08-25 07:29:53 -0400

    Write first test; gutter balls.

--- bowling/test/bowling_test.rb
+++ bowling/test/bowling_test.rb
@@ -5,5 +5,8 @@ require 'minitest/pride'
 require_relative '../lib/bowling'
 
 class BowlingTest < Minitest::Test
-
+  def test_gutter_game
+    rolls = [0] * 20
+    assert_equal 0, Bowling.new(rolls).score
+  end
 end
