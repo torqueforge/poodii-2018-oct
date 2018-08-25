1071fe1a9050b81835f0738588ecfdcd34611efc
Sandi Metz <sandi@sandimetz.com>
2018-08-25 07:47:37 -0400

    Make the 1 spare test pass.
    The code now differentiates between spares and open frames, and
    uses different rules to calculate their scores.

--- bowling/lib/bowling.rb
+++ bowling/lib/bowling.rb
@@ -5,6 +5,25 @@ class Bowling
   end
 
   def score
-    rolls.sum
+    running_score = 0
+    current_frame = 0
+    max_frames    = 10
+    remaining_rolls = rolls
+
+    while current_frame < max_frames
+      current_frame += 1
+
+      # spare
+      if (remaining_rolls.take(2).sum) == 10
+        running_score  += remaining_rolls.take(3).sum
+        remaining_rolls = remaining_rolls.drop(2)
+      end
+
+      # open frame
+      running_score  += remaining_rolls.take(2).sum
+      remaining_rolls = remaining_rolls.drop(2)
+    end
+
+    running_score
   end
 end
