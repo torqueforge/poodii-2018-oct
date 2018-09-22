6e19396458fa4d497700bd8be3dfe3fa77a28620
Sandi Metz <sandi@sandimetz.com>
2018-09-22 04:44:04 -0400

    No-tap requirement

--- bowling/lib/bowling.rb
+++ bowling/lib/bowling.rb
@@ -13,6 +13,21 @@ class Bowling
     while current_frame < max_frames
       current_frame += 1
 
+      # New Requirement: No-tap,
+      #   where a strike/spare requires fewer than 10 pins.
+      #
+      # So, the 10's need to vary.
+      # The sets of code below are identical _except_ for the 'magic' numbers.
+      # If the numbers had names, you express every rule with common code like:
+      #
+      # if (remaining_rolls.take(_num_triggering_rolls).sum) >= _triggering_value
+      #   if remaining_rolls.size >= _num_rolls_to_score
+      #     running_score += remaining_rolls.take(_num_rolls_to_score).sum
+      #     remaining_rolls = remaining_rolls.drop(_num_triggering_rolls)
+      #   end
+      #   next
+      # end
+
       # strike
       if (remaining_rolls.take(1).sum) == 10
         if remaining_rolls.size > 2
