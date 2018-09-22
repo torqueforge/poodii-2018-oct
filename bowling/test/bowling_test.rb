08185dd4da4050799d95883e42567bd777da8690
Sandi Metz <sandi@sandimetz.com>
2018-09-22 04:30:57 -0400

    L!: Bowling Kata Tests

---
+++ bowling/test/bowling_test.rb
@@ -0,0 +1,58 @@
+require_relative '../../test_helper'
+require_relative '../lib/bowling'
+
+class BowlingTest < Minitest::Test
+  def test_gutter_game
+    rolls = [0] * 20
+    assert_equal 0, Bowling.new(rolls).score
+  end
+
+  def test_all_ones
+    skip
+    rolls = [1] * 20
+    assert_equal 20, Bowling.new(rolls).score
+  end
+
+  def test_one_spare
+    skip
+    rolls = [5, 5, 3] + [0] * 17
+    assert_equal 16, Bowling.new(rolls).score
+  end
+
+  def test_one_strike
+    skip
+    rolls = [10, 3, 4] + [0] * 18
+    assert_equal 24, Bowling.new(rolls).score
+  end
+
+  def test_perfect_game
+    skip
+    rolls = [10] * 12
+    assert_equal 300, Bowling.new(rolls).score
+  end
+
+  # def test_final_spare
+  #   rolls = [1] * 18 + [4,6,4]
+  #   assert_equal 32, Bowling.new(rolls).score
+  # end
+
+  # def test_scoring_partial_game_with_complete_frames
+  #   rolls = [1] * 6
+  #   assert_equal 6, Bowling.new(rolls).score
+  # end
+
+  # def test_scoring_partial_game_with_incomplete_final_frame
+  #   rolls = [1] * 5
+  #   assert_equal 4, Bowling.new(rolls).score
+  # end
+
+  # def test_scoring_partial_game_with_unfulfilled_strike
+  #   rolls = [10] * 10
+  #   assert_equal 240, Bowling.new(rolls).score
+  # end
+
+  # def test_scoring_partial_game_with_unfulfilled_spare
+  #   rolls = [5,5,6,4]
+  #   assert_equal 16, Bowling.new(rolls).score
+  # end
+end
