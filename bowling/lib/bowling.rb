bafbcd8c327c0e44a0e5320847a1699b8de15e9b
Sandi Metz <sandi@sandimetz.com>
2018-09-23 11:17:31 -0400

    Add NOTAP rules, fix dang typo

--- bowling/lib/bowling.rb
+++ bowling/lib/bowling.rb
@@ -38,8 +38,11 @@ class Rules
         {num_triggering_rolls: 2, triggering_value: 10, num_rolls_to_score: 3},
         {num_triggering_rolls: 2, triggering_value:  0, num_rolls_to_score: 2} ]
       },
-    :TOTAP => {
-      :scoring_rules => [ ]
+    :NOTAP => {
+      :scoring_rules => [
+        {num_triggering_rolls: 1, triggering_value: 9, num_rolls_to_score: 3},
+        {num_triggering_rolls: 2, triggering_value: 9, num_rolls_to_score: 3},
+        {num_triggering_rolls: 2, triggering_value: 0, num_rolls_to_score: 2} ]
       }
     }
 
