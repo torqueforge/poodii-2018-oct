54b94b83e380bcf0dddee9338c080aa3b4a19395
Sandi Metz <sandi@sandimetz.com>
2018-09-23 09:12:56 -0400

    Try same thing for spare

--- bowling/lib/bowling.rb
+++ bowling/lib/bowling.rb
@@ -40,6 +40,18 @@ class Bowling
         next
       end
 
+      _num_triggering_rolls =  2
+      _triggering_value     = 10
+      _num_rolls_to_score   =  3
+
+      if (remaining_rolls.take(_num_triggering_rolls).sum) >= _triggering_value
+        if remaining_rolls.size >=  _num_rolls_to_score
+          running_score  += remaining_rolls.take(_num_rolls_to_score).sum
+          remaining_rolls = remaining_rolls.drop(_num_triggering_rolls)
+        end
+        next
+      end
+
       # spare
       if (remaining_rolls.take(2).sum) >= 10
         if remaining_rolls.size >= 3
