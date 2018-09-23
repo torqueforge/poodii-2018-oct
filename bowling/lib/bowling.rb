9105a8cfbdd2554fc6be54546efe6b3851c43596
Sandi Metz <sandi@sandimetz.com>
2018-09-23 11:27:19 -0400

    NOTAP via merge of bowling_l2_refactor
    Notice the lament in the tests

--- bowling/lib/bowling.rb
+++ bowling/lib/bowling.rb
@@ -1,7 +1,8 @@
 class Bowling
-  attr_reader :rolls
-  def initialize(rolls)
-    @rolls = rolls
+  attr_reader :rolls, :config
+  def initialize(rolls, config=Rules::CONFIGS[:TENPIN])
+    @rolls  = rolls
+    @config = config
   end
 
   def score
@@ -12,50 +13,47 @@ class Bowling
 
     while current_frame < max_frames
       current_frame += 1
+      rule = Rules.new(config: config).scoring_rule(remaining_rolls)
 
-      # New Requirement: No-tap,
-      #   where a strike/spare requires fewer than 10 pins.
-      #
-      # So, the 10's need to vary.
-      # The sets of code below are identical _except_ for the 'magic' numbers.
-      # If the numbers had names, you express every rule with common code like:
-      #
-      # if (remaining_rolls.take(_num_triggering_rolls).sum) >= _triggering_value
-      #   if remaining_rolls.size >= _num_rolls_to_score
-      #     running_score += remaining_rolls.take(_num_rolls_to_score).sum
-      #     remaining_rolls = remaining_rolls.drop(_num_triggering_rolls)
-      #   end
-      #   next
-      # end
-
-      # strike
-      if (remaining_rolls.take(1).sum) == 10
-        if remaining_rolls.size > 2
-          running_score += remaining_rolls.take(3).sum
-          remaining_rolls = remaining_rolls.drop(1)
-        end
-        next
-      end
-
-      # spare
-      if (remaining_rolls.take(2).sum) == 10
-        if remaining_rolls.size > 2
-          running_score  += remaining_rolls.take(3).sum
-          remaining_rolls = remaining_rolls.drop(2)
-        end
-        next
-      end
-
-      # open frame
-      if (remaining_rolls.take(2).sum) >= 0
-        if remaining_rolls.size >= 2
-        running_score += remaining_rolls.take(2).sum
-        remaining_rolls = remaining_rolls.drop(2)
+      if (remaining_rolls.take(rule[:num_triggering_rolls]).sum) >= rule[:triggering_value]
+        if remaining_rolls.size >=  rule[:num_rolls_to_score]
+          running_score  += remaining_rolls.take(rule[:num_rolls_to_score]).sum
+          remaining_rolls = remaining_rolls.drop(rule[:num_triggering_rolls])
         end
         next
       end
     end
 
     running_score
+  end
+end
+
+require 'ostruct'
+
+class Rules
+  CONFIGS = {
+    :TENPIN => {
+      :scoring_rules => [
+        {num_triggering_rolls: 1, triggering_value: 10, num_rolls_to_score: 3},
+        {num_triggering_rolls: 2, triggering_value: 10, num_rolls_to_score: 3},
+        {num_triggering_rolls: 2, triggering_value:  0, num_rolls_to_score: 2} ]
+      },
+    :NOTAP => {
+      :scoring_rules => [
+        {num_triggering_rolls: 1, triggering_value: 9, num_rolls_to_score: 3},
+        {num_triggering_rolls: 2, triggering_value: 9, num_rolls_to_score: 3},
+        {num_triggering_rolls: 2, triggering_value: 0, num_rolls_to_score: 2} ]
+      }
+    }
+
+  attr_reader :config
+  def initialize(config:)
+    @config = OpenStruct.new(config)
+  end
+
+  def scoring_rule(rolls)
+    config.scoring_rules.find {|rule|
+      (rolls.take(rule[:num_triggering_rolls]).sum) >= rule[:triggering_value]
+    }
   end
 end
