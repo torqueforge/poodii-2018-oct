5cead0f1ca9449a501db1ff26d5932db24981838
Sandi Metz <sandi@sandimetz.com>
2018-09-24 10:07:06 -0400

    Add turn_rule role players

--- bowling/lib/bowling.rb
+++ bowling/lib/bowling.rb
@@ -110,6 +110,19 @@ class MissingBonusRollsFrame < MissingNormalRollsFrame
   end
 end
 
+#####################################################################
+class GeneralTurnRule
+  def turn_complete?(frame)
+    frame.normal_rolls_complete?
+  end
+end
+
+class FinalFrameTurnRule
+  def turn_complete?(frame)
+    (frame.normal_rolls_complete? and frame.bonus_rolls_complete?)
+  end
+end
+
 
 #####################################################################
 class DetailedScoresheet
