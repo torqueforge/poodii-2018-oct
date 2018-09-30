bb99bbb56a86af7b8190daee32e87173f9b26a8b
Sandi Metz <sandi@sandimetz.com>
2018-09-30 17:55:48 -0400

    TDD cheater

--- bowling/test/cheater_test.rb
+++ bowling/test/cheater_test.rb
@@ -2,4 +2,28 @@ require_relative '../../test_helper'
 require_relative '../bowling'
 
 class CheaterTest < Minitest::Test
+  def setup
+    @cheater = Cheater.for(name: "Cheaty", rolls: [])
+  end
+
+  def test_cheater_leaves_rolls_of_5_or_more_untouched
+    ignorable_rolls = (5..10).to_a
+
+    ignorable_rolls.each {|r|
+      c = @cheater.new_roll(r)
+
+      assert_equal r, c.rolls.last
+    }
+  end
+
+  def test_cheater_improves_rolls_of_less_than_5
+    cheatable_rolls  = (0..4).to_a
+    substitute_score = 8
+
+    cheatable_rolls.each {|r|
+      c = @cheater.new_roll(r)
+
+      assert_equal substitute_score, c.rolls.last
+    }
+  end
 end
