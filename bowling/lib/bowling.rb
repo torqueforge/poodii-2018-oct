ecf8865db6a275667500b7d39fb924247d829d5b
Sandi Metz <sandi@sandimetz.com>
2018-10-01 07:34:01 -0400

    Delete obsolete code and comments

--- bowling/lib/bowling.rb
+++ bowling/lib/bowling.rb
@@ -6,33 +6,8 @@ class Bowling
   end
 
   def score
-    # I'd like to rely on Rules to separate rolls into frame objects
-    # which know their score, and only be responsible here for
-    # calculating the final score, like so:
-    #
     frame_list = Rules.new(config: config).framify(rolls)
     frame_list.reduce(0) {|sum, frame| sum += frame.score}
-    #
-    # Making the above code work requires adding a Frame class
-    # that knows its score, and adding #framify to Rules to
-    # return an instance of Frame.  Thus, this commit.
-
-    # running_score = 0
-    # current_frame = 0
-    # max_frames    = 10
-    # remaining_rolls = rolls
-
-    # while current_frame < max_frames
-    #   current_frame += 1
-    #   rule = Rules.new(config: config).scoring_rule(remaining_rolls)
-
-    #   if remaining_rolls.size >=  rule[:num_rolls_to_score]
-    #     running_score  += remaining_rolls.take(rule[:num_rolls_to_score]).sum
-    #     remaining_rolls = remaining_rolls.drop(rule[:num_triggering_rolls])
-    #   end
-    # end
-
-    # running_score
   end
 end
 
@@ -66,8 +41,6 @@ class Rules
     @config = OpenStruct.new(config)
   end
 
-  # This method is a copy of the Bowling#score method, adjusted to
-  # accumulate Frame objects rather than calculate a running_score.
   def framify(rolls)
     frame_list    = []
     running_score = 0
